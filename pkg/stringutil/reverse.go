57261168c595755d93901d36d97c4b9d80ca0bae
qiyongqiang <qiyongqiang@shoplineapp.com>
2023-01-04 14:30:42 +0800

    qiqiang:test

--- pkg/stringutil/reverse.go
+++ pkg/stringutil/reverse.go
@@ -1,9 +1,12 @@
 package stringutil
 
+import "fmt"
+
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
+	fmt.Printf("Reverse %s", s)
 	return string(r)
 }
